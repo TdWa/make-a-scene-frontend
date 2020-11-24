0dd520d334514522f8e052523d940712dcb12857
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-24 22:21:42 +0100

    add form to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form, StyledLink, PageTitle } from "../styles/styledElements";
 
 export default function LoginPage() {
-  return <div>Login page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // dispatch(login(email, password....));
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <div>
+      <PageTitle>Log in</PageTitle>
+      <Form onSubmit={submitForm}>
+        <div className="formRow">
+          <label htmlFor="loginEmail">Email</label>
+          <input
+            id="loginEmail"
+            name="loginEmail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          ></input>
+        </div>
+        <div className="formRow">
+          <label htmlFor="loginPassword">Password</label>
+          <input
+            id="loginPassword"
+            name="loginPassword"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          ></input>
+        </div>
+        <div className="formRow">
+          <Button type="submit">Login</Button>
+        </div>
+        <div className="formRow">
+          <StyledLink to="/signup">Or click here to make an account</StyledLink>
+        </div>
+      </Form>
+    </div>
+  );
 }
