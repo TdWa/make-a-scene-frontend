dedf8b705154c3c7a1f61c1c89078b85f9bbd596
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-24 22:21:29 +0100

    import page bgcolor from theme

--- src/styles/styledElements/MainWrapper.ts
+++ src/styles/styledElements/MainWrapper.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { ThemeType } from "../theme";
 
-export const MainWrapper = styled.main`
-  background-color: #ff6347;
+export const MainWrapper = styled.main<{ theme: ThemeType }>`
+  background-color: ${({ theme }) => theme.colors.main};
   min-height: 60vw;
 `;
