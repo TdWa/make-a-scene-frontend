89a262a7a1a4464906c5f1e6594a62e9a7aa3a0c
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-25 14:35:34 +0100

    rename reducer -> rootReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
-import reducer from "./rootReducer";
+import rootReducer from "./rootReducer";
 
 declare global {
   interface Window {
@@ -12,6 +12,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
 
-const store = createStore(reducer, enhancer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
