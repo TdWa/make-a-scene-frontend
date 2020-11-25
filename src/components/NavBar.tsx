ad3798dd6429c2b78dcd089cd57714f1b314dd7d
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-25 16:36:49 +0100

    add temporary logout button to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { Button } from "../styles/styledElements";
+import { logOut } from "../store/user/actions";
 
 export default function NavBar() {
+  const dispatch = useDispatch();
   return (
     <div>
+      <Button onClick={() => dispatch(logOut())}>Logout</Button>
+      <br></br>
+      <NavLink to="/login">Login</NavLink>
+      <br></br>
+      <br></br>
       <NavLink to="/" exact={true}>
         Home
       </NavLink>
@@ -23,15 +32,6 @@ export default function NavBar() {
       <NavLink to="/myScenes">My Scenes</NavLink>
       <br></br>
       <NavLink to="/myScene/1">MyScene 1</NavLink>
-      <br></br>
-      <br></br>
-      Login stuff:
-      <br></br>
-      <NavLink to="/signup">Signup</NavLink>
-      <br></br>
-      <NavLink to="/login">Login</NavLink>
-      <br></br>
-      <br></br>
     </div>
   );
 }
