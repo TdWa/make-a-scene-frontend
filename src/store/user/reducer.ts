f01c8d7be2578733ec7cd1ecc5388e1ab8352911
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-25 14:38:28 +0100

    update userslicereducer and types for login/signup

--- src/store/user/reducer.ts
+++ src/store/user/reducer.ts
@@ -1,12 +1,16 @@
 import {
   UserState,
   UserActionTypes,
+  LOADING_USER,
+  USER_REQUEST_ERROR,
   LOGIN_SUCCESS,
   TOKEN_STILL_VALID,
   LOG_OUT,
 } from "./types";
 
 const initialState: UserState = {
+  loading: false,
+  error: null,
   token: localStorage.getItem("token"),
   name: null,
   email: null,
@@ -14,12 +18,18 @@ const initialState: UserState = {
 
 export default (state = initialState, action: UserActionTypes) => {
   switch (action.type) {
+    case LOADING_USER:
+      return { ...state, loading: true, error: null };
+
+    case USER_REQUEST_ERROR:
+      return { ...state, loading: false, error: action.payload };
+
     case LOGIN_SUCCESS:
       localStorage.setItem("token", action.payload.token);
-      return { ...state, ...action.payload };
+      return { ...state, loading: false, error: null, ...action.payload };
 
     case TOKEN_STILL_VALID:
-      return { ...state, ...action.payload };
+      return { ...state, loading: false, error: null, ...action.payload };
 
     case LOG_OUT:
       localStorage.removeItem("token");
--- src/store/user/types.ts
+++ src/store/user/types.ts
@@ -1,37 +1,55 @@
+// state type
 export type UserState = {
+  loading: boolean;
+  error: string | null;
   token: string | null;
   name: string | null;
   email: string | null;
   about?: string;
 };
 
+// user types
 export type UserWithoutToken = {
-  name: string | null;
-  email: string | null;
-  about: string | null;
+  name: string;
+  email: string;
+  about: string;
 };
 
 export type UserWithToken = UserWithoutToken & { token: string };
 
+// action types
+export const LOADING_USER = "LOADING_USER";
+export const USER_REQUEST_ERROR = "USER_REQUEST_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const TOKEN_STILL_VALID = "TOKEN_STILL_VALID";
 export const LOG_OUT = "LOG_OUT";
 
-export type ManualLoginAction = {
+export type SetLoadingUser = {
+  type: typeof LOADING_USER;
+};
+
+export type SetUserRequestError = {
+  type: typeof USER_REQUEST_ERROR;
+  payload: string;
+};
+
+export type ManualLogin = {
   type: typeof LOGIN_SUCCESS;
   payload: UserWithToken;
 };
 
-export type TokenLoginAction = {
+export type TokenLogin = {
   type: typeof TOKEN_STILL_VALID;
   payload: UserWithoutToken;
 };
 
-export type LogoutAction = {
+export type Logout = {
   type: typeof LOG_OUT;
 };
 
 export type UserActionTypes =
-  | ManualLoginAction
-  | TokenLoginAction
-  | LogoutAction;
+  | SetLoadingUser
+  | SetUserRequestError
+  | ManualLogin
+  | TokenLogin
+  | Logout;
