bd0a9f53e7caf43fe6f34d2b59561b15e2ee40ff
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-25 14:32:19 +0100

    wrap store provider around App

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/globalStyles";
 import { theme } from "./styles/theme";
 
 ReactDOM.render(
-  <Router>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
