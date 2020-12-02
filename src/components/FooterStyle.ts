eb16d0fd6071e01e7c897dbabf4478856658aaa6
TdWa <59452572+TdWa@users.noreply.github.com>
2020-12-02 22:35:13 +0100

    update styling

--- src/components/FooterStyle.ts
+++ src/components/FooterStyle.ts
@@ -1,11 +1,22 @@
 import styled from "styled-components";
+import { ThemeType } from "../general-styles/theme";
 
-export const FooterStyle = styled.footer`
-  display: flex;
+export const FooterStyle = styled.footer<{ theme: ThemeType }>`
   background-color: black;
   color: white;
-  height: 100px;
+  height: 50px;
   margin-top: 60px;
-  align-items: center;
-  justify-content: center;
+
+  > div {
+    height: 100%;
+    max-width: ${({ theme }) => theme.widths.maxPageWidth};
+    margin: 0 auto;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    p {
+      margin-right: 70px;
+    }
+  }
 `;
--- src/pages/HomePageStyle.ts
+++ src/pages/HomePageStyle.ts
@@ -1,51 +1,38 @@
 import styled from "styled-components";
 
 export const HomePageStyle = styled.div`
-  background-color: yellow;
+  width: 100%;
 
   > div {
     margin-bottom: 50px;
   }
 
+  .welcome {
+    max-width: 600px;
+    margin-left: 20px;
+  }
+
   .homeNav {
+    margin: 0 auto 50px auto;
     display: flex;
     justify-content: space-evenly;
+    align-items: center;
+    background-color: white;
+    border-radius: 8px;
+    text-align: center;
 
-    > div {
-      height: 100px;
-      width: 40%;
-      min-width: 250px;
-      background-color: pink;
+    button {
+      margin: 50px 0;
     }
   }
+
+  .demo {
+    h2 {
+      margin-left: 20px;
+    }
+  }
+
+  p {
+    padding: 5px;
+  }
 `;
-
-// > div {
-//   margin: 20px 10px;
-// }
-
-// p {
-//   margin-left: 10px;
-// }
-
-// input,
-// select {
-//   width: 200px;
-//   padding: 3px;
-// }
-
-// .actorCreaterContainer {
-//   display: flex;
-// }
-
-// .actorCreater {
-//   margin-right: 20px;
-
-//   p {
-//     margin: 15px 0 3px 0;
-//   }
-// }
-
-// button {
-//   align-self: flex-end;
-// }
